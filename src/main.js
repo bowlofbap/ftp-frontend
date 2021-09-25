cfd4f1656d4082dfe2f82a6de217c9d45107dc96
Brett Medina <brett.medina@utexas.edu>
2021-09-24 19:55:39 -0500

    copy ronin address to clipboard.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
@@ -12,6 +13,8 @@ import { IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 Vue.use(IconsPlugin)
+Vue.use(VueClipboard)
+VueClipboard.config.autoSetContainer = true
 Vue.use(Chartkick.use(Chart))
 
 new Vue({
